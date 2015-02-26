814c2937439bdbedbf025574a19343f83f8befc9
David Fifield <david@bamsoftware.com>
2015-02-25 20:56:58 -0800

    Changes, allow only two vowels per syllable.

--- syllables.py
+++ syllables.py
@@ -7,7 +7,7 @@ CONSONANTS = "bcdfgklmnpqrstvxyz"
 VOWELS = "aehiouw"
 
 def syllables(s):
-    return re.findall(r'[*()/\\=+|' + CONSONANTS + r']?[*()/\\=+|' + VOWELS + r']+[*()/\\=+|' + CONSONANTS + ']*\'?', s)
+    return re.findall('[*()/\\=+|' + CONSONANTS + ']?[*()/\\=+|' + VOWELS + ']{1,2}[*()/\\=+|' + CONSONANTS + ']*\'?', s)
 
 def tone_of(syl):
     if "/" in syl:
@@ -18,13 +18,24 @@ def tone_of(syl):
         return "="
     return "."
 
-SENTENCE = "mousa/wn *(elikwnia/dwn a)rxw/meq' a)ei/dein,"
+# mous a/ wn *(el ik wn i a/d wn a)rx w/m eq' a) ei/d ein
+# SENTENCE = "mousa/wn *(elikwnia/dwn a)rxw/meq' a)ei/dein,"
+# SENTENCE = "h)\ *(/ippou krh/nhs h)\ *)olmeiou= zaqe/oio"
+# print SENTENCE
+# print " ".join(syllables(SENTENCE))
+
+# sys.exit()
 
 print """\
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset=utf-8>
+<style>
+p {
+    line-spacing: 0.5em;
+}
+</style>
 </head>
 <body>
 <p>
