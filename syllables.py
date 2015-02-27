783e494853977304cef56090f1784dab4c352677
David Fifield <david@bamsoftware.com>
2015-02-26 23:33:32 -0800

    Link to same tones.

--- syllables.py
+++ syllables.py
@@ -12,7 +12,7 @@ VOWELS = ur'[αΑεΕηΗιΙοΟωΩυΥ]'
 CONSONANTS = ur'[βΒξΞδΔφΦγΓκΚλΛμΜνΝπΠθΘρΡςΣσϲϹτΤϝϜχΧψΨζΖ]'
 DIACRITICALS = ur'[\u0313\u0314\u0301\u0342\u0300\u0308\u0345\u0323]'
 
-def syllables(s):
+def syllabify(s):
     return re.findall(ur'(?:%(c)s(?:’ +)?)?(?:%(v)s%(v)s%(d)s*|%(v)s%(d)s*)(?:%(c)s’?)?' % {"c": CONSONANTS, "v": VOWELS, "d": DIACRITICALS}, s, flags=re.UNICODE)
 
 def tone_of(syl):
@@ -24,6 +24,9 @@ def tone_of(syl):
         return "="
     return "."
 
+def nfc(u):
+    return unicodedata.normalize("NFC", u)
+
 print """\
 <!DOCTYPE html>
 <html>
@@ -32,6 +35,13 @@ print """\
 </head>
 <body>
 <table>
+<tr>
+<th>line</th>
+<th>tone pattern</th>
+<th>line</th>
+<!-- <th>syllabification</th> -->
+<th>same tones</th>
+</tr>
 """
 
 IMG_MAP = {
@@ -42,12 +52,21 @@ IMG_MAP = {
 }
 
 LINES = []
+TONE_PATTERN_MAP = {}
 
 with codecs.open("theogony.beta", encoding="utf-8") as f:
+    lineno = 0
     for beta_line in f:
         beta_line = beta_line.strip()
         line = beta.decode(beta_line)
         LINES.append(line)
+        if line == u"":
+            continue
+        lineno += 1
+        syllables = syllabify(line)
+        tone_pattern = "".join(tone_of(syl) for syl in syllables)
+        TONE_PATTERN_MAP.setdefault(tone_pattern, [])
+        TONE_PATTERN_MAP[tone_pattern].append(lineno)
 
 lineno = 0
 for line in LINES:
@@ -57,12 +76,21 @@ for line in LINES:
     lineno += 1
     print "<tr id=l%d>" % lineno
     print "<td align=right><a href=\"#l%d\">%d</a></td>" % (lineno, lineno)
-    print "<td>"
-    for syl in syllables(line):
-        sys.stdout.write("<img src=%s>" % IMG_MAP[tone_of(syl)])
-    print "</td>"
-    print "<td>" + unicodedata.normalize("NFC", line).encode("utf-8") + "</td>"
-    print "<td>" + u"–".join(syllables(line)).encode("utf-8") + "</td>"
+    imgs = []
+    syllables = syllabify(line)
+    for syl in syllables:
+        imgs.append("<img src=%s>" % IMG_MAP[tone_of(syl)])
+    print "<td>" + "".join(imgs) + "</td>"
+    print "<td>" + nfc(line.replace(u" ", u"&nbsp;")).encode("utf-8") + "</td>"
+    # print "<td>" + nfc(u"–".join(syllables)).encode("utf-8") + "</td>"
+    tone_pattern = "".join(tone_of(syl) for syl in syllables)
+    links = []
+    for other_lineno in TONE_PATTERN_MAP[tone_pattern]:
+        if other_lineno == lineno:
+            links.append("%d" % other_lineno)
+        else:
+            links.append("<a href=\"#l%d\">%d</a>" % (other_lineno, other_lineno))
+    print "<td>" + "&nbsp;".join(links) + "</td>"
     print "</tr>"
 
 print """
