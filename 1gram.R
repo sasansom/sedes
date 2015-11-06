78e173a7892dcca6cb5bafbee55b374174be6490
David Fifield <david@bamsoftware.com>
2015-11-05 22:35:25 -0800

    Fix y-axis of 1gram charts.

--- 1gram.R
+++ 1gram.R
@@ -9,6 +9,6 @@ x <- read.table(dat_filename, col.names=c("count", "frac", "letter"))
 
 p <- ggplot(x, aes(letter, count/sum(count)))
 p <- p + geom_bar(stat="identity")
-p <- p + scale_y_continuous(labels=percent)
+p <- p + scale_y_continuous(labels=percent, limits=c(0, 0.125))
 p <- p + labs(title=title_text, y="fraction")
 ggsave(paste(file_path_sans_ext(dat_filename), ".png", sep=""), p, width=5, height=3, dpi=90)
