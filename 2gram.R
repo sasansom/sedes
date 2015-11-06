12ca95c9e9405ff134a908f692afba2450435304
David Fifield <david@bamsoftware.com>
2015-11-06 00:16:55 -0800

    Fix the limits of 2gram graphs.

--- 2gram.R
+++ 2gram.R
@@ -9,7 +9,7 @@ x <- read.table(dat_filename, col.names=c("count", "frac", "l1", "l2"))
 
 p <- ggplot(x, aes(l1, l2, fill=count/sum(count)))
 p <- p + geom_tile()
-p <- p + scale_fill_gradient(low="white", high="steelblue", name="fraction", labels=percent)
+p <- p + scale_fill_gradient(low="white", high="steelblue", name="fraction", labels=percent, limits=c(0, 0.04))
 p <- p + labs(title=title_text)
 p <- p + theme(aspect.ratio=1)
 ggsave(paste(file_path_sans_ext(dat_filename), ".png", sep=""), p, width=6, height=5, dpi=90)
