436e3b9709ec7ae9d3de6a75fa2be8a7b93cd87f
David Fifield <david@bamsoftware.com>
2016-05-12 14:28:09 -0700

    Program to draw relative frequency of words in each position.

---
+++ graph.R
@@ -0,0 +1,23 @@
+library(ggplot2)
+library(scales)
+
+pos_sort_key <- function(pos) {
+	parts <- strsplit(as.character(pos), "/")
+	# Map "?" to a high number.
+	parts <- lapply(parts, function(x) {as.numeric(ifelse(x=="?", 100, x))})
+	# Prepend number of matches.
+	lapply(parts, function(x) {c(length(x), x)})
+}
+
+x <- read.table("n1.out", sep="\t", quote="",
+	col.names=c("work", "ref", "pos", "form", "text"),
+	colClasses=c(work="factor", ref="character", pos="factor", form="character", text="character"))
+x <- x[!grepl("/", x$pos), ]
+x <- droplevels(x)
+levels(x$pos) <- levels(x$pos)[order(as.numeric(levels(x$pos)))]
+
+p <- ggplot(x, aes(pos, group=work))
+p <- p + geom_bar(aes(y=..prop..))
+p <- p + scale_y_continuous(labels=percent)
+p <- p + facet_grid(work ~ .)
+ggsave("ἐν.png", p, dpi=120)
