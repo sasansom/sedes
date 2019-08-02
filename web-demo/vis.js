5a3d63cc2985423645a11a8076f9a35da3e27fcf
David Fifield <david@bamsoftware.com>
2019-08-01 21:18:27 -0600

    Web visualization demo.

---
+++ web-demo/vis.js
@@ -0,0 +1,29 @@
+const ALL_STYLES = [
+  "shade-text",
+  "shade-bubbles",
+  "size-bubbles",
+  "shade-background",
+];
+
+const BOOL_GRID = document.getElementById("bool-grid");
+const GROUP_STYLE = document.getElementById("group-style");
+const TEXT = document.getElementById("text");
+
+function setClass(elem, className, cond) {
+  if (cond) {
+    elem.classList.add(className);
+  } else {
+    elem.classList.remove(className);
+  }
+}
+
+BOOL_GRID.addEventListener("change", event => {
+  setClass(TEXT, "grid", event.target.checked);
+});
+setClass(TEXT, "grid", BOOL_GRID.checked);
+
+GROUP_STYLE.addEventListener("change", event => {
+  TEXT.classList.remove(...ALL_STYLES);
+  TEXT.classList.add(event.target.value);
+});
+TEXT.classList.add(GROUP_STYLE.value);
