d02f1439d7dc73fa67fe18bc57967f9e6b99bf2f
David Fifield <david@bamsoftware.com>
2020-03-03 00:02:44 -0700

    Include a metrical_shape column in the CSV.

--- src/sedes.py
+++ src/sedes.py
@@ -3,18 +3,23 @@ import re
 def assign(scansion):
     """From a metrical scansion (sequence of (character cluster, preliminary
     metrical analysis, final metrical analysis) tuples as output by
-    hexameter.scan.analyze_line_metrical), produce a sequence of (word, sedes)
-    tuples."""
+    hexameter.scan.analyze_line_metrical), produce a sequence of (word, sedes,
+    shape) tuples."""
     # Output list of tuples.
     result = []
     # List of character cluster making up the current word.
     word = []
+    # List of "-" and "+" symbols indicating the metrical shape of the current
+    # word.
+    shape = []
     # Sedes of the current word -- the first sedes seen after a word break.
     word_sedes = None
     # Buffer of words seen up until a sedes after a word break. This is needed
     # for sequences like "δ’ ἐτελείετο", where the "δ’" inherits the sedes of
     # the following word.
     words = []
+    # Buffer of metrical shapes seen up until a sedes after a word break.
+    shapes = []
     sedes = 0.0
 
     # Append a dummy word break at the end of the line, as a sentinel to output
@@ -24,6 +29,8 @@ def assign(scansion):
 
     for c, _, value in scansion:
         word.append(c)
+        if value in ("-", "+"):
+            shape.append(value)
         if " " in c:
             assert value == "", value
             assert word, word
@@ -33,13 +40,17 @@ def assign(scansion):
             word = re.sub("^[^\\w\u0313\u0314\u0301\u0342\u0300\u0308\u0345\u0323\u2019]*", "", word)
             word = re.sub("[^\\w\u0313\u0314\u0301\u0342\u0300\u0308\u0345\u0323\u2019]*$", "", word)
             words.append(word)
+            shape = "".join(shape)
+            shapes.append(shape)
             if word_sedes is not None:
                 # Once we know the sedes, output all the words seen since the
                 # last time we saw a sedes.
-                for w in words:
-                    result.append((w, "{:g}".format(word_sedes)))
+                for (w, s) in zip(words, shapes):
+                    result.append((w, "{:g}".format(word_sedes), s))
                 words = []
+                shapes = []
             word = []
+            shape = []
             word_sedes = None
 
         # If it's a vowel with a sedes value, advance the sedes counter.
