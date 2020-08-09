19e19650362058e89a4e7dd380e5858c1e44409e
David Fifield <david@bamsoftware.com>
2020-08-08 20:16:32 -0600

    Split words on any non-punctuation character, not just a space.
    The main goal of this is to treat an em dash as a word separator:
    ```
    -Argon.,1,333,5,κόσμον—ἐπαρτέα,auto,1,5,+--+--,
    +Argon.,1,333,5,κόσμον,auto,1,5,+-,ko/smos
    +Argon.,1,333,6,ἐπαρτέα,auto,1,6.5,-+--,e)parth/s
    ```
    
    ```
    -Dion.,2,159,1,"ἔσσομαι,ὡς",auto,1,1,+--+,
    +Dion.,2,159,1,ἔσσομαι,auto,1,1,+--,ei)mi/
    +Dion.,2,159,2,ὡς,auto,1,3,+,w(s
    -Dion.,33,313,4,ἀναίνεται.ἔρρε,auto,1,6.5,-+--+-,
    +Dion.,33,313,4,ἀναίνεται,auto,1,6.5,-+--,a)nai/nomai
    +Dion.,33,313,5,ἔρρε,auto,1,9,+-,e)/rrw
    -Dion.,42,158,4,τόξα;τίς,auto,1,5,+--,
    +Dion.,42,158,4,τόξα,auto,1,5,+-,to/con
    +Dion.,42,158,5,τίς,auto,1,6.5,-,ti/s
    -Dion.,43,374,4,γαμοστόλον·οὐρανόθεν,auto,1,6.5,-+--+--+,
    +Dion.,43,374,4,γαμοστόλον,auto,1,6.5,-+--,gamosto/los
    +Dion.,43,374,5,οὐρανόθεν,auto,1,9,+--+,ou)rano/qen
    ```
    
    It eliminates a spurious empty word (was `...` in the beta code).
    ```
    -Dion.,41,318,1,,auto,1,1,,
    -Dion.,41,318,2,εἰρομένῃ,auto,1,1,+--+,e)/romai
    +Dion.,41,318,1,εἰρομένῃ,auto,1,1,+--+,e)/romai
    ```
    
    It has the side effect of breaking up words that are separated by a '['.
    I'm not sure what the correct thing to do is in cases like this.
    ```
    -Hom.Hymn,2,387,3,ἑτέρ[ωθεν,auto,1,4,--+-,
    +Hom.Hymn,2,387,3,ἑτέρ,auto,1,4,--,
    +Hom.Hymn,2,387,4,ωθεν,auto,1,5,+-,
    ```
    
    Currently, `[1` and `]1` digraphs are handled wrongly, but that's a
    separate bug that will be fixed separately.
    ```
    -Callim.Hymn,3,1,1,ἄρτεμιν [1,auto,1,1,+--,
    -Callim.Hymn,3,1,2,οὐ,auto,1,3,+,ou)
    +Callim.Hymn,3,1,1,ἄρτεμιν,auto,1,1,+--,a)/rtemis
    +Callim.Hymn,3,1,2,1οὐ,auto,1,3,+,
    -Callim.Hymn,3,1,6,λαθέσθαι]1,auto,1,10.5,-++,
    +Callim.Hymn,3,1,6,λαθέσθαι1,auto,1,10.5,-++,
    ```
    
    Closes #20.

--- src/sedes.py
+++ src/sedes.py
@@ -13,6 +13,45 @@ KNOWN_SCANSIONS = dict(
     for key, value in KNOWN_SCANSIONS.items()
 )
 
+def trim(s):
+    return re.sub("[^\\w\u0313\u0314\u0301\u0342\u0300\u0308\u0345\u0323\u2019]*", "", s)
+
+# Yields sub-iterators for each contiguous group of scansion elements that
+# belong to the same word, throwing away all punctuation and non-letter
+# elements.
+def partition_scansion_into_words(scansion):
+    current = []
+    scansion = scansion[:]
+    while scansion:
+        element = scansion.pop(0)
+        c, mid, value = element
+
+        if value == "|":
+            # This is just a foot marker, ignore it.
+            continue
+
+        # If it's a space with punctuation, put the punctuation either at the
+        # end of this word or the beginning of the next, depending on what side
+        # of the space it's on.
+        parts = c.split(" ", 1)
+        trimmed = trim(parts[0])
+        if trimmed:
+            current.append((trimmed, mid, value))
+        if (len(parts) == 1 and trimmed != c) or len(parts) > 1:
+            # There's a word break here. Yield the current word.
+            if current:
+                yield current
+            current = []
+            # If there's something after a space, push it into the scansion
+            # queue to be re-handled in the next iteration as the beginning of
+            # the next word.
+            if len(parts) > 1:
+                trimmed = trim(parts[1])
+                if trimmed:
+                    scansion.insert(0, (trimmed, mid, value))
+    if current:
+        yield current
+
 def assign(scansion):
     """From a metrical scansion (sequence of (character cluster, preliminary
     metrical analysis, final metrical analysis) tuples as output by
@@ -20,11 +59,6 @@ def assign(scansion):
     shape) tuples."""
     # Output list of tuples.
     result = []
-    # List of character cluster making up the current word.
-    word = []
-    # List of "-" and "+" symbols indicating the metrical shape of the current
-    # word.
-    shape = []
     # Sedes of the current word -- the first sedes seen after a word break.
     word_sedes = None
     # Buffer of words seen up until a sedes after a word break. This is needed
@@ -35,48 +69,41 @@ def assign(scansion):
     shapes = []
     sedes = 1.0
 
-    # Append a dummy word break at the end of the line, as a sentinel to output
-    # the final group of words sharing a sedes.
-    if len(scansion) < 1 or " " not in scansion[-1][0]:
-        scansion += ((" ", "", ""),)
+    for sub_scansion in partition_scansion_into_words(scansion):
+        # List of character cluster making up the current word.
+        word = []
+        # List of "-" and "+" symbols indicating the metrical shape of the current
+        # word.
+        shape = []
+        for c, _, value in sub_scansion:
+            word.append(c)
+            if value in ("-", "+"):
+                shape.append(value)
+                if word_sedes is None:
+                    # The first vowel in a word, remember the sedes for the whole word.
+                    word_sedes = sedes
+            # If it's a vowel with a sedes value, advance the sedes counter.
+            if value == "-":
+                sedes += 0.5
+            elif value == "+":
+                sedes += 1.0
+        # Append this word to the list of words that share the current
+        # sedes.
+        word = "".join(word)
+        words.append(word)
+        shape = "".join(shape)
+        shapes.append(shape)
 
-    for c, _, value in scansion:
-        word.append(c)
-        if value in ("-", "+"):
-            shape.append(value)
-        if " " in c:
-            assert value == "", value
-            assert word, word
-            # Trim punctuation and spaces from the current word and append it to
-            # the list of words that share the current sedes.
-            word = "".join(word)
-            word = re.sub("^[^\\w\u0313\u0314\u0301\u0342\u0300\u0308\u0345\u0323\u2019]*", "", word)
-            word = re.sub("[^\\w\u0313\u0314\u0301\u0342\u0300\u0308\u0345\u0323\u2019]*$", "", word)
-            words.append(word)
-            shape = "".join(shape)
-            shapes.append(shape)
-            if word_sedes is not None:
-                # Once we know the sedes, output all the words seen since the
-                # last time we saw a sedes.
-                for (w, s) in zip(words, shapes):
-                    result.append((w, "{:g}".format(word_sedes), s))
-                words = []
-                shapes = []
-            word = []
-            shape = []
+        if word_sedes is not None:
+            # Once we know the sedes, output all the words seen since the
+            # last time we saw a sedes.
+            for (w, s) in zip(words, shapes):
+                result.append((w, "{:g}".format(word_sedes), s))
+            words = []
+            shapes = []
             word_sedes = None
 
-        if (value == "-" or value == "+") and word_sedes is None:
-            # The first vowel in a word, remember the sedes for the whole word.
-            word_sedes = sedes
-        # If it's a vowel with a sedes value, advance the sedes counter.
-        if value == "-":
-            sedes += 0.5
-        elif value == "+":
-            sedes += 1.0
-
     assert sedes == 13.0, sedes
-    assert not word, word
     assert not words, words
     assert word_sedes is None, word_sedes
     return tuple(result)
