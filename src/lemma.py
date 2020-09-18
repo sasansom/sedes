b5ea9d82a72f538123932722095504572b783f82
David Fifield <david@bamsoftware.com>
2020-09-18 13:53:57 -0600

    Permit local overrides of lemmatization.

--- src/lemma.py
+++ src/lemma.py
@@ -9,12 +9,21 @@ import unicodedata
 
 # https://docs.cltk.org/en/latest/greek.html#lemmatization-backoff-method
 from cltk.lemmatize.greek.backoff import BackoffGreekLemmatizer
+from cltk.lemmatize.backoff import DictLemmatizer
 # https://docs.cltk.org/en/latest/greek.html#normalization
 from cltk.corpus.utils.formatter import cltk_normalize
 
 __all__ = ["lookup"]
 
-lemmatizer = BackoffGreekLemmatizer()
+OVERRIDES = (
+    ("τ’", "τε"),
+)
+
+cltk_lemmatizer = BackoffGreekLemmatizer()
+# Insert our own lookup of hardcoded lemmata before the CTLK process.
+lemmatizer = DictLemmatizer(dict(
+    (map(cltk_normalize, x) for x in OVERRIDES)
+), source = "Sedes overrides", backoff = cltk_lemmatizer.lemmatizer, verbose = cltk_lemmatizer.VERBOSE)
 
 def lookup(word, default=None):
     # The CLTK lemmatizer expects its input to be normalized according to
